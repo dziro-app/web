00411d1416062f4368122de1a1fbcce8899bb6f0
everitosan <everardo.ipn@gmail.com>
2022-12-26 12:02:18 -0600

    feat: add console log for pro app

--- apps/pro/src/main.ts
+++ apps/pro/src/main.ts
@@ -5,7 +5,7 @@ const app = new App({
   target: document.getElementById('app')
 })
 
-console.log(`%c                    🄳🅉🄸🅁🄾 `, "background: #000; color: #fff; font-size: 20px")
+console.log(`%c                    🄳🅉🄸🅁🄾-pro `, "background: #000; color: #fff; font-size: 20px")
 console.log(`%c by evesan (https://www.linkedin.com/in/everitosan/)`, "background: #ebebeb; color: #0773ff;")
 
 
